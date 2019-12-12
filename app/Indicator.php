ca408a261a12ada0e21f736d2b81943f7a0ca1c8
Noval Bintang <mnovalbs@gmail.com>
2019-12-12 23:40:09 +0700

    remove patient in indicator

--- app/Indicator.php
+++ app/Indicator.php
@@ -16,9 +16,4 @@ class Indicator extends Model
     {
         return $this->belongsTo('App\User', 'user_id');
     }
-
-    public function patient()
-    {
-        return $this->belongsTo('App\Patient', 'patient_id');
-    }
 }
