9a6f2af42e6f67bd218acb0d4e9c7dc53465f136
Noval Bintang <mnovalbs@gmail.com>
2019-12-13 23:31:57 +0700

    create general panel

---
+++ resources/js/components/Panel.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="panel">
+    <div class="panel-heading">
+      <h3 class="panel-title">{{ title }}</h3>
+    </div>
+    <div class="panel-body">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Panel',
+
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
